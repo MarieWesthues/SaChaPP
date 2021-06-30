ded3745175e5388f69bf6e99e1a77c42bec684d6
MarieWesthues <marie.westhues@stud.uni-due.de>
2021-06-30 14:29:03 +0200

    projektname

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/SaChaPP'));
+app.use(express.static('./dist/sacha'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/SaChaPP/'}),
+    res.sendFile('index.html', {root: 'dist/sacha/'}),
 );
 
 // Start the app by listening on the default Heroku port
